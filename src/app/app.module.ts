0efe3a8b7768878a4602d95e4b1e2e6364fc651a
Arthus Ni <arthus8ni@gmail.com>
2021-08-04 12:28:00 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,64 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+
 import { AppComponent } from './app.component';
+import { BookCategoryComponent } from './components/book-category/book-category.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { SearchComponent } from './components/search/search.component';
+
+
+import { BookService } from './services/book.service';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
+const routes: Routes = [
+  {path: 'checkout', component: CheckoutComponent},
+  {path: 'cart-details', component: CartDetailsComponent},
+  {path: 'books/:id', component: BookDetailsComponent},
+  {path: 'books', component: BookListComponent},
+  {path: 'search/:keyword', component: BookListComponent},
+  {path: 'category/:id', component: BookListComponent},
+  {path: '', redirectTo: '/books', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookCategoryComponent,
+    BookDetailsComponent,
+    BookListComponent,
+    CartDetailsComponent,
+    CartStatusComponent,
+    CheckoutComponent,
+    PageNotFoundComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    HttpClientModule,
+    NgbModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
+
+
   ],
-  providers: [],
+  providers: [ BookService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
